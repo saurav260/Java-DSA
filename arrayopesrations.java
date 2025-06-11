8b4652ca2f36bee53f06f9fdd6f4900b0368dc4c
Saurav Doke <sauravdoke@gmail.com>
2025-06-11 13:56:12 +0530

    Arrays opeartors and reverse of an array

---
+++ arrayopesrations.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class arrayopesrations {
+    public static void main (String[]args){
+    ArrayList<Integer> list =  new ArrayList<>();
+    ArrayList<String> list2 =  new ArrayList<>();
+    ArrayList<Boolean> list3 =  new ArrayList<>();
+    
+    //Add element
+    list.add(1);
+    list.add(2);
+    list.add(3);
+    list.add(4);
+    list.add(5);
+
+    
+    // System.out.println(list);
+   
+    //  //get element
+    //  int element=list.get(2);
+    //  System.out.println(element);
+    
+
+    //  //delete element
+    //  list.remove(2);
+    //  System.out.println(list);
+
+    //  //set element at index
+    //  list.set(2,10 );
+    //  System.out.println(list);
+
+    //  //contains element
+
+    //  list.contains(1);
+    //  System.out.println(list);
+
+    //  System.out.println(list.size());
+
+    //  question reverse of array list
+    for(int i=list.size()-1;i>=0;i--){
+        System.out.print(list.get(i)+ " ");
+    }
+}
+}
